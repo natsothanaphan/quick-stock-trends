3ae8b944c145d67f3badc06cce273ce5770dc13f
Nat Sothanaphan <natsothanaphan@gmail.com>
2025-02-11 12:38:48 +0700

    small default

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,8 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-const {onRequest} = require("firebase-functions/v2/https");
+const { setGlobalOptions } = require("firebase-functions/v2");
+const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
+
+setGlobalOptions({ region: 'asia-southeast1' });
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
