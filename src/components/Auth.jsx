99f43e5c6e2034f8dd6158d9d31b18f70dd46f1b
Nat Sothanaphan <natsothanaphan@gmail.com>
2025-03-01 14:55:34 +0700

    improve auth

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,16 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { auth } from '../firebase';
-import { signInWithEmailAndPassword } from 'firebase/auth';
+import { signInWithEmailAndPassword,
+  setPersistence, browserSessionPersistence, onAuthStateChanged } from 'firebase/auth';
 import './Auth.css';
-import { alertAndLogErr } from '../utils.js';
+import { alertAndLogErr } from '../utils';
 
 const Auth = ({ onSignIn }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(true);
 
   const handleSignIn = async (e) => {
     e.preventDefault();
     try {
+      await setPersistence(auth, browserSessionPersistence);
       const userCredential = await signInWithEmailAndPassword(auth, email, password);
       onSignIn(userCredential.user);
     } catch (err) {
@@ -18,20 +21,27 @@ const Auth = ({ onSignIn }) => {
     }
   };
 
+  useEffect(() => onAuthStateChanged(auth, (user) => {
+    setLoading(false);
+    if (user) onSignIn(user);
+  }), []);
+
+  if (loading) return <p>Loading...</p>;
+
   return (
     <>
-      <form className="auth-form" onSubmit={handleSignIn}>
+      <form className='auth-form' onSubmit={handleSignIn}>
         <div>
-          <label htmlFor=".auth-email">Email</label>
-          <input id=".auth-email" type="email" value={email}
+          <label htmlFor='.auth-email'>Email</label>
+          <input id='.auth-email' type='email' value={email}
             onChange={(e) => setEmail(e.target.value)} required />
         </div>
         <div>
-          <label htmlFor=".auth-password">Password</label>
-          <input id=".auth-password" type="password" value={password}
+          <label htmlFor='.auth-password'>Password</label>
+          <input id='.auth-password' type='password' value={password}
             onChange={(e) => setPassword(e.target.value)} required />
         </div>
-        <button type="submit">Sign in</button>
+        <button type='submit'>Sign in</button>
       </form>
     </>
   );
