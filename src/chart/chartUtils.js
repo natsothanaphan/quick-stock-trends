932a7e4f16226d1b378c1bb1a88ba9742a80e6a2
Nat Sothanaphan <natsothanaphan@gmail.com>
2025-03-15 14:36:52 +0700

    better color distance

--- src/chart/chartUtils.js
+++ src/chart/chartUtils.js
@@ -3,18 +3,16 @@ if (themeMode === 'light') themeMode = 'light';
 else if (themeMode === 'dark') themeMode = 'dark';
 else themeMode = window.matchMedia('(prefers-color-scheme: light)').matches ? 'light' : 'dark';
 
-const getRandomColor = () => {
-  if (themeMode === 'light') {
-    const r = Math.floor(Math.random() * 192);
-    const g = Math.floor(Math.random() * 192);
-    const b = Math.floor(Math.random() * 192);
-    return `rgb(${r}, ${g}, ${b})`;
-  } else {
-    const r = 255 - Math.floor(Math.random() * 192);
-    const g = 255 - Math.floor(Math.random() * 192);
-    const b = 255 - Math.floor(Math.random() * 192);
-    return `rgb(${r}, ${g}, ${b})`;
-  }
+const getRandomColor = themeMode === 'light' ? () => {
+  const r = Math.floor(Math.random() * 200);
+  const g = Math.floor(Math.random() * 200);
+  const b = Math.floor(Math.random() * 200);
+  return `rgb(${r}, ${g}, ${b})`;
+} : () => {
+  const r = 255 - Math.floor(Math.random() * 200);
+  const g = 255 - Math.floor(Math.random() * 200);
+  const b = 255 - Math.floor(Math.random() * 200);
+  return `rgb(${r}, ${g}, ${b})`;
 };
 
 const parseRGB = (color) => {
@@ -23,13 +21,15 @@ const parseRGB = (color) => {
   if (start < 0 || end < 0) return null;
   return color.slice(start + 1, end).split(',').map((n) => parseInt(n.trim(), 10));
 };
+// https://en.wikipedia.org/wiki/Color_difference
 const calculateDistance = (color1, color2) => {
   const [r1, g1, b1] = color1;
   const [r2, g2, b2] = color2;
-  return Math.sqrt((r1 - r2) ** 2 + (g1 - g2) ** 2 + (b1 - b2) ** 2);
+  const rMean = (r1 + r2) / 2;
+  return Math.sqrt((2 + rMean / 256) * (r1 - r2) ** 2 + 4 * (g1 - g2) ** 2 + (2 + (255 - rMean) / 256) * (b1 - b2) ** 2) / 3;
 };
-const MAX_DISTANCE = { 2: 80, 3: 72, 4: 60 };
-const DEFAULT_MAX_DISTANCE = 50;
+const MAX_DISTANCE = { 2: 50, 3: 40, 4: 35, 5: 30 };
+const DEFAULT_MAX_DISTANCE = 25;
 const generateDistinctColorMapping = (labels) => {
   let colorMapping;
   let isValid = false;
